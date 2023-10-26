b211fe69841958192f7413a8c2af7bf00a4a1c69
Abdulaziz360 <abdulazizcw360@gmail.com>
2023-10-26 12:35:38 +0500

    Add js project

---
+++ script.js
@@ -0,0 +1,26 @@
+const buttons=document.querySelectorAll('.btn')
+
+buttons.forEach(function(button){
+    console.log(button)
+    
+        button.addEventListener('click',function(e){
+            console.log(e)
+            console.log(e.target)
+            if(e.target.id==='grey'){
+            document.body.style.backgroundColor=e.target.id
+            }
+            if(e.target.id==='white'){
+            document.body.style.backgroundColor=e.target.id
+            }
+            if(e.target.id==='blue'){
+            document.body.style.backgroundColor=e.target.id
+            }
+            if(e.target.id==='yellow'){
+            document.body.style.backgroundColor=e.target.id
+            }
+           
+
+    })
+
+
+})
